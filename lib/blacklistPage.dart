c3fd3b82fdc259f1ea6b55593a28e88950630d83
Vishal Katwe <vishk217@gmail.com>
2021-07-15 15:16:36 +1000

    Skeleton blacklist page

---
+++ lib/blacklistPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class blacklistPage extends StatefulWidget {
+  @override;
+  Widget listView(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blacklist Page'),
+      )
+    );
+  }
+}
