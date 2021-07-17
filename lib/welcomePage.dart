51fc97946105a0768a5bb815b0e41fc383eeb16b
Rishabh Dhawan <rishdvn@gmail.com>
2021-07-17 23:31:49 +1000

    Welcome Page animation

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome Page'),
-        ));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Stay",
+              style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            TypewriterAnimatedTextKit(
+                text: ["Productive!", "synonym 1!", "synonym 2!", "#onTrack"],
+                textStyle:
+                  TextStyle(fontSize: 42.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                totalRepeatCount: 4,
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true
+            )
+          ]
+        )
+      )
+    );
   }
 }
