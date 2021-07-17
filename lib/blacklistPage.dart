e8682058762bfa94cb76007b3df9f77575383e7e
Vishal Katwe <vishk217@gmail.com>
2021-07-17 13:55:40 +1000

    Added skeleton implementation for app reader

--- lib/blacklistPage.dart
+++ lib/blacklistPage.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class blacklistPage extends StatelessWidget {
+class BlacklistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: AppBar(
       title: Text('Blacklist Page'),
-    ));
+          elevation: 0,
+        ),
+      body: Container(child: Column(children: [BlacklistBox()])));
+  }
+}
+
+class BlacklistBox extends StatefulWidget {
+  @override
+  _BlacklistBoxState createState() => _BlacklistBoxState();
+}
+
+class _BlacklistBoxState extends State<BlacklistBox> {
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(children: [
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  // Some state change
+                });
+              },
+              child: Text('Click for location')),
+          Text("Lol")
+        ]));
   }
 }
