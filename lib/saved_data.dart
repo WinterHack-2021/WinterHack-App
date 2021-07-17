afcdeb646710e676d4d68d8be7f29f5f1a678fc1
lordcelp <lordcelpz@gmail.com>
2021-07-17 17:42:58 +1000

    Update saved_data.dart

--- lib/saved_data.dart
+++ lib/saved_data.dart
@@ -1,3 +1,12 @@
-var savedlocations = [];
+class Tuple<T1, T2> {
+  final T1 a;
+  final T2 b;
 
+  Tuple(this.a, this.b);
+}
+
+var savedlocations = [
+  Tuple('University', true),
+  Tuple('School', false),
+];
 var loadedapps = [];
