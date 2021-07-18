e26994d0531a674b0c72fc04fa804e61540f0a12
Vishal Katwe <vishk217@gmail.com>
2021-07-18 20:05:16 +1000

    Mainactivity channel link

--- android/app/src/main/kotlin/com/winterhack/winterhack_2021/MainActivity.kt
+++ android/app/src/main/kotlin/com/winterhack/winterhack_2021/MainActivity.kt
@@ -2,9 +2,13 @@ package com.winterhack.winterhack_2021
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.app.ActivityManager.RunningTaskInfo
+
+import android.content.Context
+import android.app.ActivityManager
+
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "blacklist-channel"
@@ -26,6 +30,27 @@ class MainActivity : FlutterActivity() {
     }
 
     private fun disablerEnabler(): String {
-        return "aOWIUDBIUFB"
+        return "Suuup";
+//        String currentRunningApp = TaskChecker.getForegroundApplication(yourContext);
+//
+//        if(currentRunningApp.equals("com.whatsapp")){
+//            ActivityManager am = (ActivityManager)yourContext.getSystemService(Context.ACTIVITY_SERVICE);
+//
+//            Intent startMain = new Intent(Intent.ACTION_MAIN);
+//            startMain.addCategory(Intent.CATEGORY_HOME);
+//            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            this.startActivity(startMain);
+//
+//            am.killBackgroundProcesses(currentRunningApp );
+//        }
     }
 }
+//
+//public class TaskChecker{
+//    public static String getForegroundApplication(Context context){
+//        ActivityManager am=(ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+//        RunningTaskInfo foreground=am.getRunningTasks(1).get(0);
+//        return foreground.topActivity.getPackageName();
+//    }
+//}
+
