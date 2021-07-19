33ff544e4ff9581641714e2b3a2c0cc3fd068ef8
lordcelp <lordcelpz@gmail.com>
2021-07-19 10:46:24 +1000

    added imports for places and implementation for andriodx appcompactactivity

--- android/app/src/main/kotlin/com/winterhack/winterhack_2021/MainActivity.kt
+++ android/app/src/main/kotlin/com/winterhack/winterhack_2021/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.winterhack.winterhack_2021
-
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import android.app.ActivityManager.RunningTaskInfo
-
 import android.content.Context
 import android.app.ActivityManager
+
+import android.Manifest.permission
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.Nullable
+import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+
+
 import com.google.android.libraries.places.api.Places
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.PlaceLikelihood
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
 
 
 class MainActivity : FlutterActivity() {
@@ -56,15 +71,14 @@ class MainActivity : FlutterActivity() {
 
         // Create a new PlacesClient instance
         val placesClient = Places.createClient(this)
-        return "yoyo";
-
+        
 
 
         // Use fields to define the data types to return.
-        // val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
+        //val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
 
         // // Use the builder to create a FindCurrentPlaceRequest.
-        // val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+        //val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
 
         // // Call findCurrentPlace and handle the response (first check that the user has granted permission).
         // if (ContextCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) ==
@@ -92,13 +106,12 @@ class MainActivity : FlutterActivity() {
         //     // See https://developer.android.com/training/permissions/requesting
         //     getLocationPermission()
         // }
+        
+        return "yoyo";
     }
 
     
 }
-
-
-
 //
 //public class TaskChecker{
 //    public static String getForegroundApplication(Context context){
@@ -107,4 +120,3 @@ class MainActivity : FlutterActivity() {
 //        return foreground.topActivity.getPackageName();
 //    }
 //}
-
