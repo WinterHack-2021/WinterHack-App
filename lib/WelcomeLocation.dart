67c31f8f48f8f710f33cf6dd9a3d3c651330d781
Ryan <ryan2002.rs@gmail.com>
2021-07-20 14:08:42 +1000

    Fix launching issues

--- lib/WelcomeLocation.dart
+++ lib/WelcomeLocation.dart
@@ -2,67 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-class WelcomePage extends StatefulWidget {
+class WelcomeLocation extends StatefulWidget {
   @override
-  _WelcomePageState createState() => _WelcomePageState();
+  _WelcomeLocationState createState() => _WelcomeLocationState();
 }
 
-class _WelcomePageState extends State<WelcomePage> {
+class _WelcomeLocationState extends State<WelcomeLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 50),
-                  Text(
-                    "Stay",
-                    style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.normal),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  AnimatedTextKit(
-                      animatedTexts: [
-                        TyperAnimatedText(
-                            'Productive!',
-                            textStyle:
-                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
-                        ),
-                        TyperAnimatedText(
-                            'Focused!',
-                            textStyle:
-                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
-                        ),
-                        TyperAnimatedText(
-                            'Concentrated!',
-                            textStyle:
-                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
-                        ),
-                        ColorizeAnimatedText(
-                            '#onTrack',
-                            textStyle:
-                            TextStyle(fontSize: 45.0, fontFamily: 'Horizon', fontWeight: FontWeight.bold),
-                            colors: [
-                              Colors.purple,
-                              Colors.blue,
-                              Colors.yellow,
-                              Colors.red,
-                            ]
-                        )
-                      ],
-                      totalRepeatCount: 1
-                  ),
-                ]
-            ),
-          ),
-        )
+        body: Text("Hello")
     );
   }
 }
-
-// Container(alignment: Alignment.bottomRight, child: Row(children: [Text('SWIPE'),IconButton(onPressed: () {},icon: Icon(Icons.arrow_right_alt_outlined),iconSize: 60.0,),],),),
