f1c1941185f892b9a6fd0f841c908de6d078be08
Rishabh Dhawan <rishdvn@gmail.com>
2021-07-20 14:03:11 +1000

    Add Welcome page changes

--- lib/WelcomeLocation.dart
+++ lib/WelcomeLocation.dart
@@ -2,11 +2,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-class WelcomeLocation extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.blue
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 50),
+                  Text(
+                    "Stay",
+                    style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                            'Productive!',
+                            textStyle:
+                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
+                        ),
+                        TyperAnimatedText(
+                            'Focused!',
+                            textStyle:
+                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
+                        ),
+                        TyperAnimatedText(
+                            'Concentrated!',
+                            textStyle:
+                            TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)
+                        ),
+                        ColorizeAnimatedText(
+                            '#onTrack',
+                            textStyle:
+                            TextStyle(fontSize: 45.0, fontFamily: 'Horizon', fontWeight: FontWeight.bold),
+                            colors: [
+                              Colors.purple,
+                              Colors.blue,
+                              Colors.yellow,
+                              Colors.red,
+                            ]
+                        )
+                      ],
+                      totalRepeatCount: 1
+                  ),
+                ]
+            ),
+          ),
+        )
     );
   }
 }
+
+// Container(alignment: Alignment.bottomRight, child: Row(children: [Text('SWIPE'),IconButton(onPressed: () {},icon: Icon(Icons.arrow_right_alt_outlined),iconSize: 60.0,),],),),
--- lib/initial.dart
+++ lib/initial.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:winterhack_2021/WelcomeApps.dart';
 import 'package:winterhack_2021/WelcomeLocation.dart';
 import 'package:winterhack_2021/welcomePage.dart';
@@ -10,14 +13,47 @@ class Initial extends StatefulWidget {
 }
 
 class _InitialState extends State<Initial> {
+
+  PageController pageController = PageController(initialPage: 0);
+  int pageChanged = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(children: [
-        WelcomePage(),
-        WelcomeLocation(),
-        WelcomeApps(),
-      ],
+      body: Column(
+        children: [
+          Expanded(
+            flex: 10,
+            child: PageView(
+              controller: pageController,
+              onPageChanged: (index){
+                setState(() {
+                  pageChanged = index;
+                });
+              },
+              children: [
+                WelcomePage(),
+                WelcomeLocation(),
+                WelcomeApps(),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(onPressed: (){
+                    pageController.animateToPage(pageChanged++, duration: Duration(milliseconds: 150), curve: Curves.bounceInOut);
+                  }, icon: Icon(Icons.arrow_forward_ios, color: Colors.black45)),
+                  SizedBox(width: 15)
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
