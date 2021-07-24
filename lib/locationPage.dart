68cbd1879fb9aef3a855cdfb80647e0efb398206
lordcelp <lordcelpz@gmail.com>
2021-07-24 17:21:43 +1000

    building location page with nice ui and googel places autofill (incomplete)

--- lib/locationPage.dart
+++ lib/locationPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart';
+import 'geofencing.dart';
 import 'package:installed_apps/installed_apps.dart';
 import 'package:installed_apps/app_info.dart';
 
@@ -56,41 +57,12 @@ class _LocationBoxState extends State<LocationBox> {
     setState(() {});
   }
 
-  void _getUserPosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    Position userLocation = await Geolocator.getCurrentPosition();
-    setState(() {
-      position = userLocation;
-    });
-  }
-
   Widget build(BuildContext context) {
     return Container(
         child: Column(children: [
       TextButton(
           onPressed: () {
-            setState(() {
-              _getUserPosition();
-            });
+            setState(() {});
           },
           child: Text('Click for location')),
       Text(this.position.toString()),
