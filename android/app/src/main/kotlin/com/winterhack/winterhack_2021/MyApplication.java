b42995a6f9538617e94b6cb5235bbdfb50d51497
lordcelp <lordcelpz@gmail.com>
2021-07-24 12:26:52 +1000

    removed old geolocation api

--- android/app/src/main/kotlin/com/winterhack/winterhack_2021/MyApplication.java
+++ android/app/src/main/kotlin/com/winterhack/winterhack_2021/MyApplication.java
@@ -7,15 +7,12 @@ import android.os.Build;
 import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
-import io.flutter.plugins.geofencing.GeofencingService;
 
-public class MyApplication extends FlutterApplication implements PluginRegistrantCallback {
+public class MyApplication extends FlutterApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        //Abhi's Code
-        GeofencingService.setPluginRegistrant(this);
         //Vishal's Code
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel = new NotificationChannel("winterhack-channel","Messages", NotificationManager.IMPORTANCE_LOW);
@@ -23,8 +20,5 @@ public class MyApplication extends FlutterApplication implements PluginRegistran
             manager.createNotificationChannel(channel);
         }
     }
-    //Abhi's Code
-    public void registerWith(PluginRegistry registry) {registry.registrarFor("io.flutter.plugins.geofencing.GeofencingService");
-  }
 
 }
