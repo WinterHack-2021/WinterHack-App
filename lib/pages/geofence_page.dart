14c6083329ae4b432aa0fca07da14b87336f677e
Ryan <ryan2002.rs@gmail.com>
2021-07-25 22:59:13 +1000

    Add circles

--- lib/pages/geofence_page.dart
+++ lib/pages/geofence_page.dart
@@ -16,20 +16,20 @@ import '../placesapi.dart';
 
 const platform = const MethodChannel('winterhack-channel');
 
-class GeoFencePage extends StatefulWidget {
-  const GeoFencePage({Key key}) : super(key: key);
+class GeoFenceWidget extends StatefulWidget {
+  const GeoFenceWidget({Key key}) : super(key: key);
 
   @override
-  _GeoFencePageState createState() => _GeoFencePageState();
+  _GeoFenceWidgetState createState() => _GeoFenceWidgetState();
 }
 
-class _GeoFencePageState extends State<GeoFencePage> {
+class _GeoFenceWidgetState extends State<GeoFenceWidget> {
   Completer<GoogleMapController> mapController = Completer();
   StreamSubscription locationSubscription;
   TextEditingController radiusController = new TextEditingController();
   TextEditingController locationtextController = new TextEditingController();
   double radius;
-  String savename;
+  String saveName;
   Place currentPlace;
 
   void addGeofence(String geofencename, double long, double lat, double radius,
@@ -42,7 +42,6 @@ class _GeoFencePageState extends State<GeoFencePage> {
   void initState() {
     super.initState();
     final placeBloc = Provider.of<PlaceBloc>(context, listen: false);
-    getGeofences();
 
     locationSubscription = placeBloc.selectedLocation.stream.listen((place) {
       if (place != null) {
@@ -61,25 +60,32 @@ class _GeoFencePageState extends State<GeoFencePage> {
     super.dispose();
   }
 
-  void setCircle(lat, lng, rad) {
-    circles = Set.from([
-      Circle(circleId: CircleId('1'), radius: rad, center: LatLng(lat, lng))
-    ]);
-  }
-
-  var currentgeofences;
-
-  void getGeofences() async {
-    currentgeofences = await bg.BackgroundGeolocation.geofences;
+  void setCircle(BuildContext context, bool listen) {
+    var model = Provider.of<GlobalModel>(context, listen: listen);
+    circles = Set();
+    if (currentPlace != null) {
+      circles.add(Circle(
+          circleId: CircleId('currently_adding'),
+          radius: radius,
+          center: LatLng(currentPlace.geometry.location.lat,
+              currentPlace.geometry.location.lng)));
+    }
+    for (var value in model.savedLocations.items) {
+      circles.add(Circle(
+          circleId: CircleId(value.locationName),
+          radius: value.radius,
+          center: LatLng(value.lat, value.long),
+          strokeColor: value.isEnabled ? Colors.green : Colors.red));
+    }
   }
 
   void locationtext(name) {
-    locationtextController.text = '$name';
-    print('name');
-    locationtextController.selection = TextSelection.fromPosition(
-      TextPosition(offset: locationtextController.text.length),
-    );
-    setState(() {});
+    setState(() {
+      locationtextController.text = '$name';
+      locationtextController.selection = TextSelection.fromPosition(
+        TextPosition(offset: locationtextController.text.length),
+      );
+    });
   }
 
   bool show = true;
@@ -89,167 +95,160 @@ class _GeoFencePageState extends State<GeoFencePage> {
 
   Widget build(BuildContext context) {
     final placeBloc = Provider.of<PlaceBloc>(context);
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text('Locations'),
-          backgroundColor: Colors.grey.shade900,
-        ),
-        body: Container(
-            child: Column(children: [
-          Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 15, bottom: 0),
-              child: TextField(
-                onTap: () {
-                  show = true;
-                },
-                onChanged: (value) => placeBloc.searchPlaces(value),
-                controller: locationtextController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    labelText: 'Location',
-                    suffixIcon: Icon(Icons.search)),
-              )),
-          Stack(children: [
-            (placeBloc.currentLocation == null)
-                ? Center(child: CircularProgressIndicator())
-                : Container(
-                    height: 300,
-                    margin: EdgeInsets.only(
-                        top: 20, left: 10, right: 10, bottom: 10),
-                    alignment: Alignment.center,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: GoogleMap(
-                            circles: circles,
-                            myLocationEnabled: true,
-                            myLocationButtonEnabled: true,
-                            onMapCreated: (GoogleMapController controller) {
-                              mapController.complete(controller);
-                            },
-                            initialCameraPosition: CameraPosition(
-                              target: LatLng(placeBloc.currentLocation.latitude,
-                                  placeBloc.currentLocation.longitude),
-                              zoom: 11.0,
-                            )))),
-            if (placeBloc.searchResults != null &&
-                placeBloc.searchResults.length != 0 &&
-                show == true)
-              Container(
-                height: 200,
-                margin: EdgeInsets.only(left: 10, right: 10, top: 0),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.9),
-                    backgroundBlendMode: BlendMode.darken,
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            if (placeBloc.searchResults != null &&
-                placeBloc.searchResults.length != 0 &&
-                show == true)
-              Container(
-                  height: 200,
-                  margin: EdgeInsets.only(left: 10, right: 10, top: 0),
-                  child: ListView.builder(
-                    itemCount: placeBloc.searchResults.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(placeBloc.searchResults[index].description),
-                        onTap: () {
-                          var p = placeBloc.searchResults[index].description;
-                          placeBloc.setSelectedLocation(
-                              placeBloc.searchResults[index].placeId);
-                          placeBloc.clearSearches();
-                          locationtext(p);
-                          placeBloc.clearSearches();
-                          show = !show;
-                        },
-                      );
-                    },
-                  )),
-          ]),
-          Column(children: [
-            Container(
-                height: 45,
-                margin: EdgeInsets.only(right: 10, left: 10, top: 8, bottom: 8),
-                child: TextField(
-                  onChanged: (value) {
-                    savename = value;
-                  },
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      labelText: 'Save Name: This will appear in the app'),
-                )),
-            Container(
-                height: 45,
+    setCircle(context, true);
+    return Container(
+        child: Column(children: [
+      Container(
+          margin: EdgeInsets.only(left: 10, right: 10, top: 15, bottom: 0),
+          child: TextField(
+            onTap: () {
+              show = true;
+            },
+            onChanged: (value) => placeBloc.searchPlaces(value),
+            controller: locationtextController,
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                labelText: 'Location',
+                suffixIcon: Icon(Icons.search)),
+          )),
+      Stack(children: [
+        (placeBloc.currentLocation == null)
+            ? Center(child: CircularProgressIndicator())
+            : Container(
+                height: 300,
                 margin:
-                    EdgeInsets.only(right: 10, left: 10, top: 8, bottom: 20),
-                child: TextField(
-                  controller: radiusController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      labelText: 'Radius (m)'),
-                  onChanged: (t) {
-                    radius = double.parse(radiusController.text);
-                    setCircle(currentPlace.geometry.location.lat,
-                        currentPlace.geometry.location.lng, radius);
-                    setState(() {});
-                  },
-                )),
-          ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Consumer<GlobalModel>(
-                builder: (context, value, child) {
-                  return Container(
-                      margin: EdgeInsets.only(left: 0),
-                      child: TextButton(
-                          style: ButtonStyle(
-                              minimumSize:
-                                  MaterialStateProperty.all(Size(150, 60)),
-                              backgroundColor: MaterialStateProperty.all(
-                                  Colors.grey.shade700)),
-                          onPressed: () {
-                            if (radius != null && savename != null)
-                              setState(() {
-                                addGeofence(
-                                    savename,
-                                    currentPlace.geometry.location.lng,
-                                    currentPlace.geometry.location.lat,
-                                    radius,
-                                    value);
-                              });
-                          },
-                          child: Text(
-                            'Add Location',
-                            style: TextStyle(color: Colors.white),
-                          )));
-                },
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: 20),
-                  child: TextButton(
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(150, 60)),
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.grey.shade700)),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          new CupertinoPageRoute(
-                              builder: (ctxt) => AddedLocations()));
-                    },
-                    child: Text('Saved Locations',
-                        style: TextStyle(color: Colors.white)),
-                  ))
-              //ClickableLocationContainer(),  ],)
-            ],
+                    EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+                alignment: Alignment.center,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: GoogleMap(
+                        circles: circles,
+                        myLocationEnabled: true,
+                        myLocationButtonEnabled: true,
+                        onMapCreated: (GoogleMapController controller) {
+                          mapController.complete(controller);
+                        },
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(placeBloc.currentLocation.latitude,
+                              placeBloc.currentLocation.longitude),
+                          zoom: 11.0,
+                        )))),
+        if (placeBloc.searchResults != null &&
+            placeBloc.searchResults.length != 0 &&
+            show == true)
+          Container(
+            height: 200,
+            margin: EdgeInsets.only(left: 10, right: 10, top: 0),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.9),
+                backgroundBlendMode: BlendMode.darken,
+                borderRadius: BorderRadius.circular(10)),
           ),
-        ])));
+        if (placeBloc.searchResults != null &&
+            placeBloc.searchResults.length != 0 &&
+            show == true)
+          Container(
+              height: 200,
+              margin: EdgeInsets.only(left: 10, right: 10, top: 0),
+              child: ListView.builder(
+                itemCount: placeBloc.searchResults.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(placeBloc.searchResults[index].description),
+                    onTap: () {
+                      var p = placeBloc.searchResults[index].description;
+                      placeBloc.setSelectedLocation(
+                          placeBloc.searchResults[index].placeId);
+                      placeBloc.clearSearches();
+                      locationtext(p);
+                      placeBloc.clearSearches();
+                      show = !show;
+                    },
+                  );
+                },
+              )),
+      ]),
+      Column(children: [
+        Container(
+            height: 45,
+            margin: EdgeInsets.only(right: 10, left: 10, top: 8, bottom: 8),
+            child: TextField(
+              onChanged: (value) {
+                saveName = value;
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Save Name: This will appear in the app'),
+            )),
+        Container(
+            height: 45,
+            margin: EdgeInsets.only(right: 10, left: 10, top: 8, bottom: 20),
+            child: TextField(
+              controller: radiusController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  labelText: 'Radius (m)'),
+              onChanged: (t) {
+                setState(() {
+                  radius = double.parse(radiusController.text);
+                  setCircle(context, false);
+                });
+              },
+            )),
+      ]),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Consumer<GlobalModel>(
+            builder: (context, value, child) {
+              return Container(
+                  margin: EdgeInsets.only(left: 0),
+                  child: TextButton(
+                      style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(Size(150, 60)),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.grey.shade700)),
+                      onPressed: () {
+                        if (radius != null && saveName != null)
+                          setState(() {
+                            addGeofence(
+                                saveName,
+                                currentPlace.geometry.location.lng,
+                                currentPlace.geometry.location.lat,
+                                radius,
+                                value);
+                          });
+                      },
+                      child: Text(
+                        'Add Location',
+                        style: TextStyle(color: Colors.white),
+                      )));
+            },
+          ),
+          Container(
+              margin: EdgeInsets.only(left: 20),
+              child: TextButton(
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(Size(150, 60)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.grey.shade700)),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      new CupertinoPageRoute(
+                          builder: (ctxt) => AddedLocations()));
+                },
+                child: Text('Saved Locations',
+                    style: TextStyle(color: Colors.white)),
+              ))
+          //ClickableLocationContainer(),  ],)
+        ],
+      ),
+    ]));
   }
 
   Future<void> goToPlace(Place place) async {
@@ -259,6 +258,19 @@ class _GeoFencePageState extends State<GeoFencePage> {
             LatLng(place.geometry.location.lat, place.geometry.location.lng),
         zoom: 14.0)));
     setState(() {});
+  }
+}
+
+class GeoFencePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Locations'),
+          backgroundColor: Colors.grey.shade900,
+        ),
+        body: GeoFenceWidget());
   }
 }
 
--- lib/pages/onboarding/welcome_location.dart
+++ lib/pages/onboarding/welcome_location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:winterhack_2021/pages/geofence_page.dart';
 import 'package:winterhack_2021/widgets/selector_card.dart';
 
 class WelcomeLocation extends StatefulWidget {
@@ -12,72 +13,12 @@ class _WelcomeLocationState extends State<WelcomeLocation> {
 
   @override
   Widget build(BuildContext context) {
-    Function(bool selected) onChanged=(selected)=>setState(()=>isActive=selected);
+    Function(bool selected) onChanged =
+        (selected) => setState(() => isActive = selected);
     return Scaffold(
         body: Padding(
-      padding: EdgeInsets.fromLTRB(30, 80, 30, 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text('Locations',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.0,
-              )),
-          SizedBox(height: 20),
-          Text(
-            'LOCATIONS AT WHICH YOU WISH FOR DISTRACTING APPS TO DISABLE',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              height: 1.4,
-              fontSize: 13,
-              letterSpacing: 0.5,
-            ),
-          ),
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                SelectorCardWidget(
-                  name: "University",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "Work",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "School",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "McDonalds",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "Vishal's Crib",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "Office",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-                SelectorCardWidget(
-                  name: "Optiver's bank",
-                  isActive: isActive,
-                  onChanged: onChanged,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      padding: EdgeInsets.fromLTRB(30, 0, 30, 30),
+      child: SingleChildScrollView(child: GeoFenceWidget()),
     ));
   }
 }
