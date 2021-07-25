5638c06225d5087466482f3439980348ee5c8e49
Ryan <ryan2002.rs@gmail.com>
2021-07-25 13:44:47 +1000

    Clean up welcome page and reduce duplication

--- lib/blacklistPage.dart
+++ lib/blacklistPage.dart
@@ -9,24 +9,7 @@ import 'package:winterhack_2021/selector_card.dart';
 
 import 'data/schema.dart';
 
-const platform = const MethodChannel('winterhack-channel');
-
-class BlacklistPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => Blacklist();
-}
-
-class Blacklist extends State<BlacklistPage> {
-  List<AppInfo> appList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    InstalledApps.getInstalledApps(true, true).then((value) => setState(() {
-          appList = value;
-        }));
-  }
-
+class BlacklistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -39,43 +22,65 @@ class Blacklist extends State<BlacklistPage> {
               child: Text("Disabled Apps", style: theme.textTheme.headline3)),
           Text("Select the apps you wish to disable:",
               style: theme.textTheme.headline6),
-          Expanded(
-            child: Consumer<GlobalModel>(
-              builder: (context, value, child) {
-                final sortedAppList = List.of(appList);
-                // Sort the list so switches that are on are at the top
-                // otherwise, preserve initial alphabetical ordering.
-                sortedAppList.sort((a, b) {
-                  final containsA = value.disabledApps.contains(a.packageName);
-                  final containsB = value.disabledApps.contains(b.packageName);
-                  if ((containsA && containsB) || (!containsA && !containsB))
-                    return 0;
-                  return containsA && !containsB ? -1 : 1;
-                });
-                return ListView(
-                  children: [
-                    ...sortedAppList.map((app) => SelectorCardWidget(
-                          icon: app.icon,
-                          name: app.name ?? "Unknown name",
-                          onChanged: (selected) {
-                            if (app.name == null || app.packageName == null)
-                              return;
-                            if (selected)
-                              value.disabledApps.upsert(
-                                  App(app.name!, app.packageName!, selected));
-                            else
-                              value.disabledApps.remove(app.packageName!);
-                          },
-                          isActive: app.packageName != null &&
-                              ((value.disabledApps.get(app.packageName!) ??
-                                  false)),
-                        ))
-                  ],
-                );
-              },
-            ),
-          ),
+          AppListWidget(),
         ],
+      ),
+    );
+  }
+}
+
+class AppListWidget extends StatefulWidget {
+  @override
+  State createState() {
+    return _AppList();
+  }
+}
+
+class _AppList extends State<AppListWidget> {
+  List<AppInfo> appList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    InstalledApps.getInstalledApps(true, true).then((value) => setState(() {
+          appList = value;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Consumer<GlobalModel>(
+        builder: (context, value, child) {
+          final sortedAppList = List.of(appList);
+          // Sort the list so switches that are on are at the top
+          // otherwise, preserve initial alphabetical ordering.
+          sortedAppList.sort((a, b) {
+            final containsA = value.disabledApps.contains(a.packageName);
+            final containsB = value.disabledApps.contains(b.packageName);
+            if ((containsA && containsB) || (!containsA && !containsB))
+              return 0;
+            return containsA && !containsB ? -1 : 1;
+          });
+          return ListView(
+            children: [
+              ...sortedAppList.map((app) => SelectorCardWidget(
+                    icon: app.icon,
+                    name: app.name ?? "Unknown name",
+                    onChanged: (selected) {
+                      if (app.name == null || app.packageName == null) return;
+                      if (selected)
+                        value.disabledApps
+                            .upsert(App(app.name!, app.packageName!, selected));
+                      else
+                        value.disabledApps.remove(app.packageName!);
+                    },
+                    isActive: app.packageName != null &&
+                        ((value.disabledApps.get(app.packageName!) ?? false)),
+                  ))
+            ],
+          );
+        },
       ),
     );
   }
